d41079a9dc8962a60c6a56ce98f2a30a8bb4aca6
tuanca <tuanca@vng.com.vn>
2020-07-24 13:59:26 +0700

    feat: lazy load Button component

--- packages/button/src/Button.js
+++ packages/button/src/Button.js
@@ -2,7 +2,7 @@ import React, { Suspense } from 'react';
 import styles from './button.module.scss';
 import icon from './info.svg';
 
-const LazyComponent = React.lazy(() => import('./LazyComponent'))
+const LazyComponent = React.lazy(() => import('./LazyComponent'));
 
 function Button({ children, onClick }) {
   return (
@@ -12,9 +12,7 @@ function Button({ children, onClick }) {
     >
       <img className={styles.img} src={icon} height="22px" width="22px" />
       {children || 'Button'}
-      <Suspense fallback={<div>Loading...</div>}>
-        <LazyComponent />
-      </Suspense>
+      <LazyComponent />
     </div>
   );
 }
--- packages/button/src/index.pkg.js
+++ packages/button/src/index.pkg.js
@@ -1,3 +1,5 @@
-import Button from './Button';
+import React from 'react';
+
+const Button = React.lazy(() => import('./Button'));
 
 export { Button };
