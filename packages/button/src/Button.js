6295122b7f68a2630b841f48095dd35689234398
tuanca <tuanca@vng.com.vn>
2020-07-24 14:37:15 +0700

    feat: lazy-load LazyComponent when show

--- packages/button/src/Button.js
+++ packages/button/src/Button.js
@@ -1,18 +1,29 @@
-import React, { Suspense } from 'react';
+import React, { useState, useCallback } from 'react';
 import styles from './button.module.scss';
 import icon from './info.svg';
 
 const LazyComponent = React.lazy(() => import('./LazyComponent'));
 
 function Button({ children, onClick }) {
+  const [show, setShow] = useState(false);
+
+  const handleClick = useCallback(() => {
+    setShow(!show);
+    onClick && onClick();
+  }, [show]);
+
   return (
     <div
       className={styles.container}
-      onClick={onClick}
+      onClick={handleClick}
     >
       <img className={styles.img} src={icon} height="22px" width="22px" />
       {children || 'Button'}
-      <LazyComponent />
+      {
+        show ? (
+          <LazyComponent />
+        ) : null
+      }
     </div>
   );
 }
